057713e929e61bd02ce2938d58d3fc8a6e73545d
chguo <gcleon96@gmail.com>
2025-04-06 01:01:23 +0200

    REINFORCE for cartpole

--- vpg/CartPole/reinforce.py
+++ vpg/CartPole/reinforce.py
@@ -1,12 +1,48 @@
 from collections import deque
 import torch
+import numpy as np
 
 def reinforce(
         n_eps=1000, 
         max_t=1000, 
         gamma=1.0, 
         print_every=100,
+        env=None,
         policy=None,
         optimizer=None
     ):
     scores_deque = deque(maxlen=100)
+    scores = []
+    for i in range(1, n_eps+1):
+        saved_log_probs = []
+        rewards = []
+        state = env.reset()
+        for t in range(max_t):
+            action, log_prob = policy.act(state)
+            saved_log_probs.append(log_prob)
+            state, reward, done, _ = env.step(action)
+            rewards.append(reward)
+            if done:
+                break
+        scores_deque.append(sum(rewards))
+        scores.append(sum(rewards))
+
+        discounts = [gamma**i for i in range(len(rewards)+1)]
+        R = sum([a*b for a, b in zip(discounts, rewards)])
+
+        policy_loss = []
+        for log_prob in saved_log_probs:
+            policy_loss.append(-log_prob * R)
+        policy_loss = torch.cat(policy_loss).sum()
+
+        optimizer.zero_grad()
+        policy_loss.backward()
+        optimizer.step()
+
+        if i % print_every == 0:
+            print(f"Episode: {i}, average score: {np.mean(scores_deque)}.")
+        if np.mean(scores_deque) >= 195.0:
+            print(f"Cartpole env solved in {i} episodes! Average score: {np.mean(scores_deque)}.")
+            break
+    return scores
+        
--- vpg/CartPole/test.py
+++ vpg/CartPole/test.py
@@ -1,2 +1,26 @@
 import numpy as np
 import torch
+from policy import Policy
+import time
+import gym
+import matplotlib.pyplot as plt
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+policy = Policy(device=device)
+policy.load_state_dict(torch.load("policy_cartpole.pth"))
+
+env = gym.make('CartPole-v0')
+state = env.reset()
+img = plt.imshow(env.render(mode='rgb_array'))
+for t in range(1000):
+    action, _ = policy.act(state)
+    env.render()
+    time.sleep(0.1)
+
+    state, reward, done, _ = env.step(action)
+    if done:
+        break
+env.close()
+
+
